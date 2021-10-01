2bce7cb2692b775f90738a7c7808d137deb26d41
ningaard <82536882+ningaard@users.noreply.github.com>
2021-10-01 13:57:51 +0200

    Update dk.lua

--- Locals/esx_skins/dk.lua
+++ Locals/esx_skins/dk.lua
@@ -1,6 +1,6 @@
 Locales['dk'] = {
   ['skin_menu'] = 'Karakter menu',
-  ['use_rotate_view'] = 'brug ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ og ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ for at rotere.',
+  ['use_rotate_view'] = 'Brug ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ og ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ for at rotere.',
   ['skin'] = 'Ændre stilen',
   ['saveskin'] = 'Gem indstillinger',
 }
