5ef7d3c7d1eec811bea2e75d03651545efface65
ningaard <82536882+ningaard@users.noreply.github.com>
2021-10-01 13:57:16 +0200

    Update dk.lua

--- Locals/esx_rpchat/dk.lua
+++ Locals/esx_rpchat/dk.lua
@@ -1,12 +1,12 @@
 Locales['dk'] = {
   ['ooc_prefix'] = 'OOC | %s',
-  ['twt_help'] = 'send et tweet',
+  ['twt_help'] = 'Send et tweet',
   ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
-  ['me_help'] = 'personal action',
-  ['me_prefix'] = 'me | %s',
+  ['me_help'] = 'Personlig handling',
+  ['me_prefix'] = 'Me | %s',
   ['do_help'] = 'RP information',
   ['do_prefix'] = 'do | %s',
-  ['generic_argument_name'] = 'besked',
-  ['generic_argument_help'] = 'beskeden',
+  ['generic_argument_name'] = 'Besked',
+  ['generic_argument_help'] = 'Beskeden',
   ['unknown_command'] = '^3%s^0 er en ugyldig værdi!',
 }
